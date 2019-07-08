2f880fcc8a89582c08e53e4c650ab635a7d77b90
d5storm <murilo@DESKTOP-593I0VV.localdomain>
2019-07-08 01:18:32 +0000

    changed machine data structure

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ for (( a = 2; a <= 2; a++))
 do
 	for file in "${wkf[@]}"
 	do
-		for ((  i = 1 ;  i <= 10000;  i++  )) 
+		for ((  i = 1 ;  i <= 1;  i++  )) 
 		do	
 			echo  -ne $file '\n' & EXEC $execpath -x 5 -a $a -s $i -w ./input/gpu/"gpu.dag" # >> ./results/local_search_testing/"GRASP_new_LS.txt"
 			# EXEC $execpath -a $a -s $i -c $clusters -w $workflows$file$dag >> ./results/local_search_testing/"GRASP_new_LS.txt"
